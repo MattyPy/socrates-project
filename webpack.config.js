78de5f0d32409e21f40e0d7c239595ab42cb27f8
Matthew Payne <shotofan@gmail.com>
2018-02-08 22:40:06 -0700

    Get webpack sort of working

--- webpack.config.js
+++ webpack.config.js
@@ -4,12 +4,12 @@ var webpack = require('webpack');
 var BundleTracker = require('webpack-bundle-tracker');
 
 module.exports = {
-    context: __dirname,
-    entry: './assets/js/index',
+    context: path.join(__dirname, 'socrates'),
+    entry: './assets/js/index.js',
     output: {
-	path: path.resolve('./assets/webpack_bundles/'),
+	path: path.resolve('./socrates/assets/webpack_bundles/'),
 	filename: '[name] - [hash].js'
-    }
+    },
 
     plugins: [
 	new BundleTracker({filename: './webpack-stats.json'})
